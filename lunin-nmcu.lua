6662b3ca252f1c0af5561bd6b47037480fe1850f
Florian Heimgaertner <florian@heimgaertner.net>
2016-05-11 18:40:00 +0200

    Support for multiple DS18B20 sensors with NodeMCU

--- lunin-nmcu.lua
+++ lunin-nmcu.lua
@@ -1,18 +1,44 @@
 #!/usr/bin/lua
--- Lunin: Lua implementation of a munin-node
+-- munin thermometer using DS18B20 sensors on ESP8622
 -- (c) 2016, Florian Heimgaertner
 
 -- This software may be modified and distributed under the terms
 -- of the MIT license.  See the LICENSE file for details.
 
+ver  = '0.04n'
+owpin = 2
 
 require('ds18b20')
-ds18b20.setup(2)
-
-ver  = '0.04n'
+ds18b20.setup(owpin)
 
 plugin = {}
 action = {}
+
+function lunin_init()
+  local fwVer = ('NodeMCU %i.%i.%i'):format(node.info())
+  local hostname = ('esp%06x'):format(node.chipid())
+  local sensors = {}
+
+  ow.setup(owpin)
+  ow.reset_search(owpin)
+  local nilcount = 0
+  repeat
+    local addr = ow.search(owpin)
+    if (addr == nil) then
+      nc = nc + 1
+    elseif (addr:byte(1) == 0x28) then
+      table.insert(sensors, addr)
+    end
+    tmr.wdclr()
+  until (nc > 1)
+  ow.reset_search(owpin)
+
+  return hostname, fwVer, sensors
+end
+
+hostname, fwVer, sensors = lunin_init()
+
+
 
 -- munin node actions
 
@@ -42,11 +68,11 @@ action['list'] = function(socket)
 end
 
 action['version'] = function(socket)
-  socket:send('munins node on nodemcu version: '..ver..' (lunin, '.._VERSION..')\n')
+  socket:send('munins node on '..hostname..' version: '..ver..' (lunin, '.._VERSION..'/'..fwVer..')\n')
 end
 
 action['nodes'] = function(socket)
-  socket:send('nodemcu\n.\n')
+  socket:send(hostname..'\n.\n')
 end
 
 action['help'] = function(socket)
@@ -61,11 +87,18 @@ end
 
 plugin['ds18b20'] = function(socket, x)
   if x == 'conf' then
-    socket:send('graph_title DS18B20 Temperature Sensor\ngraph_vlabel degrees Celsius\ngraph_category sensors\ntemp.label temperature\n')
+    socket:send('graph_title DS18B20 Temperature Sensors\ngraph_vlabel degrees Celsius\ngraph_category sensors\n')
+    for k,v in pairs(sensors) do 
+      local saddr = ("%02x%02x%02x%02x%02x%02x"):format(v:byte(2,7))
+      socket:send('temp'..saddr..'.label '..saddr..'\n')
+    end
   else
-    local temp = ds18b20.read()
-    if temp < 85 then
-      socket:send('temp.value '..temp..'\n')
+    for k,v in pairs(sensors) do 
+      local temp, temp2 = ds18b20.readNumber(v)
+      if temp < 85 then
+	local saddr = ("%02x%02x%02x%02x%02x%02x"):format(v:byte(2,7))
+	socket:send('temp'..saddr..'.value '..temp..string.format(".%04u", temp2)..'\n')
+      end
     end
   end
 end
@@ -73,7 +106,7 @@ end
 
 srv=net.createServer(net.TCP,10)
 srv:listen(4949,function(socket)
-  socket:send('# munin node at nodemcu\n')
+  socket:send('# munin node at '..hostname..'\n')
   socket:on("receive",function(socket,payload)
     local cmd, arg = payload:match('^(%l+)%s?([%w_]*)\r?\n?$')
       if action[cmd] then
